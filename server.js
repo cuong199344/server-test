27493702479cc384d7abe49042ac01a8f983c0c9
cuong199344 <cuonghaxer2003@gmail.com>
2024-06-13 16:20:32 +0700

    test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 8800;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Dev Server running on port: ${PORT}`);
 });
+
+export default server;
